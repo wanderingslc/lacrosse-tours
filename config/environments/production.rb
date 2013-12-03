f597cf8f200f6c65b2de1e8bd8cfc32ee5419a78
Stan Martineau <wanderingslc@gmail.com>
2013-12-02 17:22:16 -0700

    Mailer move

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,13 +66,13 @@ LacrosseTours::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-        :address        =>"smtp.gmail.com",
-        :port           => 587,
-        :domain         =>"splitdodgetours.com",
-        :user_name      =>ENV["GMAIL_USERNAME"],
-        :password       =>ENV["GMAIL_PASSWORD"],
-        :authentication =>'plain',
-        :enable_starttls_auto =>true
+        address:          "smtp.gmail.com",
+        port:             587,
+        domain:           "splitdodgetours.com",
+        user_name:        ENV["GMAIL_USERNAME"],
+        password:         ENV["GMAIL_PASSWORD"],
+        authentication:   'plain',
+        enable_starttls_auto: true
 
     }
 end
