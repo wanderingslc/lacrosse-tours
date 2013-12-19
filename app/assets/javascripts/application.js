d7468593bb78f4f1ed72b43dfb69b822e1982dd0
Stan Martineau <wanderingslc@gmail.com>
2013-12-18 18:45:24 -0700

    Change the font so it sizes right on mobile devices. Also start incorporating facebook like and share buttons. Put in trial place to see if client wants there or somewhere else.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,4 +20,21 @@ $(document).ready(function(){
   var bg_image_div = $('#background-img');
   // console.debug();
   $.backstretch(bg_image_div.data('imgpath'));
+
+  //facebook like button 
+  $(function() {
+        likebutton =
+            '<fb:like href="" send="" width="100" show_faces="" ' +
+            'font="" layout="button_count" action="recommend">' +
+            '</fb:like>';
+        $.getScript('http://connect.facebook.net/en_US/all.js', function() {
+                FB.init({appId: 141936272547391,
+                            status: true,
+                            cookie: true,
+                            xfbml: true
+                            });
+                $('#facebook').replaceWith(likebutton);
+        });
+    });
 });
+
