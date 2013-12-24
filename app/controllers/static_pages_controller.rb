775af29663d4e1037c35e126121a2ef1c8bef1cb
Stan Martineau <wanderingslc@gmail.com>
2013-12-24 16:39:30 -0700

    change to fragment caching for picture carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  caches_page :home, :bus, :itinerary, :tour, :pricing, :stadium, :about, :contact, :sign_up
+  #caches_page :home, :bus, :itinerary, :tour, :pricing, :stadium, :about, :contact, :sign_up
   def home
   end
 
