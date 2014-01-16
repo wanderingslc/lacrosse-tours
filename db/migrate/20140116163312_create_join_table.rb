4d6e2d11d12bda29bb3d58b5229917e0c2672754
Stan Martineau <wanderingslc@gmail.com>
2014-01-16 09:47:14 -0700

    Change the join table migration and run again. Everything works now.

--- db/migrate/20140116163312_create_join_table.rb
+++ db/migrate/20140116163312_create_join_table.rb
@@ -1,7 +1,15 @@
 class CreateJoinTable < ActiveRecord::Migration
   def up
+    create_table :roles_users, :id => false do |t|
+      t.integer :role_id
+      t.integer :user_id
+    end
   end
 
   def down
+    destroy_table :roles_users, :id => false do |t|
+      t.integer :role_id
+      t.integer :user_id
+    end
   end
 end
