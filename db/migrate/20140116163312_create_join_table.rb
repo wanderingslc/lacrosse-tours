a284f34669e61070ba0aa5d22260c19357a33e77
Stan Martineau <wanderingslc@gmail.com>
2014-01-16 09:33:43 -0700

    Create join between roles and users table

---
+++ db/migrate/20140116163312_create_join_table.rb
@@ -0,0 +1,7 @@
+class CreateJoinTable < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140115180302) do
+ActiveRecord::Schema.define(:version => 20140116163312) do
 
   create_table "activities", :force => true do |t|
     t.string   "time"
